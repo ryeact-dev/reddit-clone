37db4fcbe0aec956d1c38759b65626204e70135f
kilvencodes <kilven.coder01@gmail.com>
2024-06-12 22:17:44 +0800

    Complete Create Topic Form

--- src/actions/create-topic.js
+++ src/actions/create-topic.js
@@ -1,5 +1,67 @@
 'use server';
 
-export async function createTopic() {
-  // TODO: revalidate the homepage
+import { z } from 'zod';
+import { auth } from '@/auth';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
+import paths from '@/paths';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formState, formData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be logged in to do this'],
+      },
+    };
+  }
+
+  let topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Unknown error'],
+        },
+      };
+    }
+  }
+
+  revalidatePath('/');
+  redirect(paths.topicShow(topic.slug));
 }

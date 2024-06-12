433ec21272d458271ceb7f517ee83574a4fd3815
kilvencodes <kilven.coder01@gmail.com>
2024-06-12 20:25:00 +0800

    refactor server actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,6 @@
-'use server';
+export { singOut } from './sing-out';
+export { signIn } from './sign-in';
 
-import * as auth from '@/auth';
-
-export async function signIn() {
-  return auth.signIn('github');
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { createComment } from './create-comment';
+export { createPost } from './create-post';
+export { createTopic } from './create-topic';

4ffd28664f0019b0507ba1d4b0199a4a8cb0ffce
kilvencodes <kilven.coder01@gmail.com>
2024-06-13 21:22:40 +0800

    Complete Create Post API and UI

--- src/actions/create-post.js
+++ src/actions/create-post.js
@@ -1,5 +1,79 @@
 'use server';
 
-export async function createPost() {
-  // TODO:revalidate the topic show page
+import { z } from 'zod';
+import { auth } from '@/auth';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
+import paths from '@/paths';
+
+const createPostSchema = z.object({
+  title: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  content: z.string().min(10),
+});
+
+export async function createPost(slug, formState, formData) {
+  const result = createPostSchema.safeParse({
+    title: formData.get('title'),
+    content: formData.get('content'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be logged in to do this'],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ['Topic not found'],
+      },
+    };
+  }
+
+  let post;
+  try {
+    post = await db.post.create({
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        topicId: topic.id,
+        userId: session.user.id,
+      },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        _form: ['Failed to create post'],
+      };
+    }
+  }
+
+  revalidatePath(paths.topicShow(slug));
+  redirect(paths.postShow(slug, post.id));
 }
--- src/app/topics/[slug]/page.jsx
+++ src/app/topics/[slug]/page.jsx
@@ -1,5 +1,18 @@
+import PostCreateForm from '@/components/posts/post-create-form';
 import React from 'react';
 
-export default function TopicShowPage() {
-  return <div>Topic Show</div>;
+export default function TopicShowPage({ params }) {
+  const { slug } = params;
+
+  return (
+    <div className='grid grid-cols-4 gap-4 p-4'>
+      <div className='col-span-3'>
+        <h1 className='text-2xl font-bold mb-2'>{slug}</h1>
+      </div>
+
+      <div>
+        <PostCreateForm slug={slug} />
+      </div>
+    </div>
+  );
 }
